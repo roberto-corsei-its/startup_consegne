309f38b2a69dd23044d334bf4a436a253afd76d3
roberto-corsei-its <rcorsei@studenti.apuliadigital.it>
2026-06-08 10:53:43 +0200

    Inizializzato blueprint consegne, inserita la registrazione del blueprint nell'app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,9 @@
-import
+from flask import Flask
+from src.routes import consegne_bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(consegne_bp)
+
+    return app
---
+++ src/routes.py
@@ -0,0 +1,5 @@
+from flask import Blueprint, request, jsonify
+
+
+
+consegne_bp = Blueprint('consegne', __name__, url_prefix='/consegne')
