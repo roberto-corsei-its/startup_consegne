fc01bd9257ca4c518e2155e5b5b4ad5a5d4f464e
roberto-corsei-its <rcorsei@studenti.apuliadigital.it>
2026-06-10 15:32:23 +0200

    fixed issues with controls

--- handlers/handler_recensioni.py
+++ handlers/handler_recensioni.py
@@ -13,12 +13,14 @@ from src.utils import view_rider, inser_review, elimin_recensione, view_reviews,
 #POST
 def inserimento_recensione(rider_id:int, customer_name:str, rating:int, comment:str):
     try:
-       lista_rider = view_rider()
-       if rider_id not in [rider[1] for rider in lista_rider]:
-          return jsonify({"Error:":"Il rider selezionato non esiste, inserisci un id valido."})
-       else:
-        inser_review(rider_id,customer_name,rating,comment)
-        return jsonify({"Message:":"Success"}), 200
+        lista_rider = view_rider()
+        if rider_id not in [rider[1] for rider in lista_rider]:
+            inser_review(rider_id,customer_name,rating,comment)
+            return jsonify({"Message:":"Success"}), 200
+         
+        else:
+            return jsonify({"Error:":"Il rider selezionato non esiste, inserisci un id valido."})
+
     except ValueError:
         return jsonify({"Error:":"I dati inseriti non sono validi, assicurati di inserire un numero intero per rider_id e rating."}), 400
     except Exception as e:
@@ -32,10 +34,11 @@ def eliminazione_recensione(rider_id:int):
        riders = view_rider()
        reviews = view_reviews()
        if rider_id not in [rider[0] for rider in riders] and rider_id not in [review[1] for review in reviews]:
-          elimin_recensione(id)
-          return jsonify({"Message:":"Recensione eliminata con successo."}), 200
-       else:
             return jsonify({"Error:":"La recensione selezionata non esiste, inserisci un id valido."}), 400
+       else:
+            elimin_recensione(id)
+            return jsonify({"Message:":"Recensione eliminata con successo."}), 200
+            
     except ValueError:
         return jsonify({"Error:":"I dati inseriti non sono validi, assicurati di inserire un numero intero per id."}), 400
     except Exception as e:
